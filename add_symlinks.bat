af3439669885ef8d03a11d943ed983764274ddd3
IlluzyonistCode <vitalijko4805go@gmail.com>
2026-02-10 15:00:37 +0400

    Fix

--- add_symlinks.bat
+++ add_symlinks.bat
@@ -1,2 +1,6 @@
+@echo off
+
 mklink /J node_modules\gitbook-plugin-pageview-count gitbook-plugin-pageview-count
 mklink /J node_modules\gitbook-plugin-fontsettings gitbook-plugin-fontsettings
+
+echo Symlinks created for Saw: Wolvesville
---
+++ add_symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -sf ../gitbook-plugin-pageview-count node_modules/gitbook-plugin-pageview-count
+ln -sf ../gitbook-plugin-fontsettings node_modules/gitbook-plugin-fontsettings
+
+echo "Symlinks created for Saw: Wolvesville"
